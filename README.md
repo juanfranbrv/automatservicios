98dbbe57899a1f28b75fccc0f860354777b5be5f
juanfranbrv <juanfranbrv@gmail.com>
2024-11-27 11:32:51 +0100

    readme

---
+++ README.md
@@ -0,0 +1,4 @@
+# Automatizacion de facturas de servicios
+
+Prueba de ls capacidades de Streamlit.  
+Espera hasta 4 PDFs, extrae su contenido y se lo pasa al modelo gemma2-9b-it (GroqCloud) para que encuentre una serie de datos (importe, fecha) en el y los devuelva en JSON. Finalmente crea una sencilla hoja de calculo para descargar.
--- app.py
+++ app.py
@@ -1,3 +1,11 @@
+'''
+═══════════════════════════════════
+Prueba de ls capacidades de Streamlit.  
+Espera hasta 4 PDFs, extrae su contenido y se lo pasa al modelo gemma2-9b-it (GroqCloud) para que encuentre una serie de datos (importe, fecha) en el y los devuelva en JSON. Finalmente crea una sencilla hoja de calculo para descargar.
+═══════════════════════════════════
+
+'''
+
 import streamlit as st
 import pdfplumber
 import pandas as pd
